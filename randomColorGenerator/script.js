1a38af1835804d8fa9a2071d546ffad2ba17c599
eveah-codes <52373568+eveah-codes@users.noreply.github.com>
2023-02-02 13:57:12 +0100

    Updated code with slider and rgb conversion

--- randomColorGenerator/script.js
+++ randomColorGenerator/script.js
@@ -1,23 +1,50 @@
 //select DOM elements
+let button = document.querySelector("button");
 let body = document.querySelector("body");
 let h1 = document.querySelector("h1");
-let button = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let slider = document.querySelector("input");
 
-//create characters
-const characters = "0123456789abcdef";
+//create a range of characters for hex code
+const characters = "0123456789ABCDEF";
 
-//generate random color function
+//function to create random color
 function generateRandomColor() {
-    let randomColor = "#";
-    for (let i = 0; i < 6; i++){
-        randomColor += characters[Math.floor(Math.random() * characters.length)];
-    }
-    return randomColor;
+  //variable with # symbol
+  let color = "#";
+  //loop through characters
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return color;
+}
+function hexToRgb(hex) {
+  let r = parseInt(hex.substring(1, 3), 16);
+  let g = parseInt(hex.substring(3, 5), 16);
+  let b = parseInt(hex.substring(5, 7), 16);
+
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
-//add event listener to button
+function hexToRgba(hex, opacity) {
+  let r = parseInt(hex.substring(1, 3), 16);
+  let g = parseInt(hex.substring(3, 5), 16);
+  let b = parseInt(hex.substring(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+let hex = "";
 button.addEventListener("click", () => {
-    let newColor = generateRandomColor();
-    h1.textContent = newColor;
-    body.style.backgroundColor = newColor;
+  hex = generateRandomColor();
+  h1.textContent = hex;
+  body.style.backgroundColor = hex;
+  h2.textContent = hexToRgb(hex);
+});
+
+slider.addEventListener("input", function () {
+  let opacity = slider.value / 100;
+  let newShade = hexToRgba(hex, opacity);
+  body.style.backgroundColor = newShade;
+  h2.textContent = newShade;
 });
